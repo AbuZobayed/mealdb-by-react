29da5894cb65502be8672d3658600d1b602a980e
AbuZobayed <zobayed.ict05@gmail.com>
2022-03-25 12:43:10 +0600

    show data

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import Header from './components/Header/Header';
 
 function App() {
   return (
-    <div className="App">
+    <div>
       <Header></Header>
       <Foods></Foods>
     </div>
--- src/components/Foods/Foods.js
+++ src/components/Foods/Foods.js
@@ -1,16 +1,24 @@
 import React, { useEffect, useState } from 'react';
+import './Foods.css'
 
 const Foods = () => {
     const [meals , setMeals] = useState([]);
 
     useEffect(() => {
         fetch("https://www.themealdb.com/api/json/v1/1/filter.php?c=Seafood")
-          .then((res) => res.json())
-          .then((data) => console.log(data.meals));
+          .then(res => res.json())
+          .then(data => setMeals(data.meals));
       }, []);
     return (
-        <div>
-            
+        <div className='foods-container'>
+            <div className="meals-container">
+               <h2>Fooods : {meals.length}</h2>
+                
+            </div>
+
+            <div className="cart-container">
+                    <h4>Orders summery</h4>
+                </div>
         </div>
     );
 };
